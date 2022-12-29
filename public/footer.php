88e4bb14a34a1356333f41dcb91279351cef2805
hoffmann michel <michel.hof@hotmail.fr>
2022-12-29 11:08:21 +0100

    test

---
+++ public/footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <div class="wrapper"> 
+
+   <div class="img3">
+    <img class="img2" src="../images/logo.png" /></h2>
+   </div>
+    
+    <div>
+        <p class="copyright">Copyright © 2023 Site réalisé par Michel Hoffmann. <br> Tous droits réservés.</p>
+    </div>
+   </div>
+  
+</footer>
+<script src="../js/caroussel.js"></script>
+
+</body>
+</html>
---
+++ public/tourisme.php
@@ -0,0 +1,33 @@
+<?php include("header.php") ?>
+
+<h1 style="text-align:center; font-size: 40px;color: gray;">Loin de tout, proche de l'étientiel</h1>
+
+<div class="clear"></div>
+
+
+
+
+
+<section id="slideshow2">
+
+	<div class="container2">
+		<div class="c_slider2"></div>
+		<div class="slider2">
+
+			<figure2>
+            <video width="730" height="410" controls autoplay>
+      <source src="../videos/besbre.mp4" type=video/mp4>
+      <div class="attribution-block"><a href="http://dig.ccmixter.org/files/Radioontheshelf/63894">Yesterdays Blues</a> by Radioontheshelf (c) copyright 2021 Licensed under a Creative Commons <a href="http://creativecommons.org/licenses/by/3.0/">Attribution (3.0)</a> license. Ft: Mr Yesterday & The Lovely Ladies</div>
+        </video>
+            </figure2>
+
+        </div>
+       </div>
+</div>
+</section>
+
+<div class="logoTourisme2">
+    <img class="logoTourisme" src="../images/logoTourisme.png" alt="" />
+    <div class="clear"></div>
+</div>
+<?php include("footer.php") ?>
