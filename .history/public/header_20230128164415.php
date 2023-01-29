6575ba0c737366bca7fd0e9f9890b927ee3aeb15
hoffmann michel <michel.hof@hotmail.fr>
2023-01-29 13:07:16 +0100

    mise en page index

---
+++ .history/public/header_20230128164415.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Nous vous proposons des hébergements en locations dans un camping à Dompierre sur Besbre dans le département de  l'Allier à 5 km du par d'attraction le Pal.">
+    <title>Location d'Hebergements Proche Du parc D'Attraction Le Pal Dans L'Allier</title>
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" type="text/css" href="../css/caroussel.scss">
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+
+          
+    </div>
+  </div>
+</nav>
+
+
+
+<div id="mainNavigation">
+  <nav role="navigation">
+    <div class="py-3 text-center border-bottom">
+      <img src="../images/logo.png" alt="" style="height:160px " class="invert img-fluid">
+      <div class=" navbar-brand fs-2 textLogo" href="#">Les Caravanes De La Besbre <span class="orange">.</span></div>
+    </div>
+ 
+  <div class="navbar-expand-md ">
+    <div class=" text-center my-2 ">
+      <button class="navbar-toggler w-75" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon hamburger " >Menu </span> <span class="align-middle"></span>
+      </button>
+    </div>
+    <div class="text-center mt-3 collapse navbar-collapse " id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="informations.php">Informations</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="tourisme.php">Tourisme</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.lepal.com/">Vente Billets</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link " #contact  href="#contact">Contact</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</div>
+
+
+<div class="container-fluid text-center m-0">
+<img src="../images/banner3.png" alt="image" class="banner ">
+</div>
+
+
+
+  
+
+          
+
+
+
+
