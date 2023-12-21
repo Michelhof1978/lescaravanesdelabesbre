2a542fbb50fc33f71aa0c4d14f436f521a42471d
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2023-12-21 10:13:01 +0100

    corection des erreurs terminé

---
+++ .history/public/camping_20231221095207.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+<!-- Linking BoxIcon for Icon -->
+<!-- <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'> -->
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" type="image/png" href="../images/logo.png">
+<link href="../css/style.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="icon" type="image/x-icon" href="../images/logo.ico">
+<meta name="description" content="Vous souhaitez organiser un séjour au Parc d'Attractions le Pal à plusieurs, possibilité de louer nos 3 caravanes placées côte à côte.">
+ <title>Camping Dompierre Sur Besbre Proche Du Pal</title>
+
+</head>
+
+ 
+
+
+<body>              
+<?php include("header.php") ?>
+
+
+<h4 class="m-5 text-center border border-3 rounded  text-white p-2 display-6 h4Index"><strong>Camping Dompierre Sur Besbre Proche Du Pal</strong></h4>
+
+<!-- Destination Start -->
+<div class="container-fluid ">
+    <div class="container pb-3">
+        <div class="text-center mb-3 pb-3">
+            <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">Destination Nature</h6>
+            <h1>Camping De Dompierre Sur Besbre</h1>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class=" position-relative overflow-hidden mb-2">
+                    <img class="img-fluid" src="../images/camping6.webp" alt="Camping Le Pal">
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class=" position-relative overflow-hidden mb-2">
+                    <img class="img-fluid" src="../images/camping7.webp" alt="Camping Dompierre sur Besbre">
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class=" position-relative overflow-hidden mb-2">
+                    <img class="img-fluid" src="../images/camping8.webp" alt="Camping Auvergne">
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+<!-- Destination Start -->
+
+<h4 class=" text-center border border-3 rounded  text-white p-2 display-6 h4Index"><strong>NOS CARAVANES</strong></h4>
+
+
+<!-- About Start -->
+<div class="container-fluid py-5 sectionTourime">
+    <div class="container ">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="position-relative h-100 pt-5">
+                    <img class=" w-100" src="../images/affiche.webp" alt="Affiche camping le pal" style="object-fit: cover;">
+                    <img class=" w-100 pt-5" src="../images/oli.webp" alt="mascotte camping Dompierre sur besbre" style="object-fit: cover;">
+                </div>
+            </div>
+            <div class="col-lg-6 pb-lg-5">
+                <div class="about-text bg-white p-lg-5 my-lg-5">
+                    <h6 class="text-primary text-uppercase pt-3" style="letter-spacing: 5px;">A LOUER</h6>
+                    <h3 class="mb-3">Possibilité de louer plusieurs caravanes pour familles nombreuses</h3>
+
+                    <p class="lead">Vous êtes une famille nombreuse ou un groupe de 8 à 12 personnes et vous cherchez un endroit confortable pour votre séjour au <strong><a href="https://www.lepal.com/">Parc d'Attractions Le Pal </a></strong>? Nous avons la solution pour vous!
+                        Nous proposons la location de trois caravanes adjacentes pour répondre à vos besoins.
+                        Avec notre équipe à votre disposition, nous ferons tout notre possible pour que votre séjour soit une réussite.</p>
+
+
+                    <div class="row mb-4">
+                        <div class="col-6">
+                            <img class="img-fluid" src="../images/camping.webp" alt="Caravanes Camping Dompierre sur besbre">
+                        </div>
+                        <div class="col-6">
+                            <img class="img-fluid" src="../images/camping1.webp" alt="aravane Camping Dompierre sur besbre">
+                        </div>
+                        <hr>
+
+                        <div class="col-6">
+                            <img class="img-fluid" src="../images/camping2.webp" alt="Caravanes Camping Le Pal">
+                        </div>
+
+                        <div class="col-6">
+                            <img class="img-fluid" src="../images/camping12.webp" alt="Location Camping Dompierre sur besbre">
+                        </div>
+                        <hr>
+
+                        <div class="col-6">
+                            <img class="img-fluid" src="../images/camping4.webp" alt="Hébergement le pal">
+                        </div>
+
+                        <div class="col-6">
+                            <img class="img-fluid" src="../images/camping5.webp" alt="Location Le pal">
+                        </div>
+
+                        <div class="col-6 pt-3">
+                            <img class="img-fluid" src="../images/camping9.webp" alt="Hébergement le pal">
+                        </div>
+
+                        <div class="col-6 p-3">
+                            <img class="img-fluid" src="../images/camping10.webp" alt="Location Le pal">
+                        </div>
+
+                        <div class="col-6 pt-3">
+                            <img class="img-fluid" src="../images/camping11.webp" alt="Hébergement le pal">
+                        </div>
+
+                        <div class="col-6 p-3">
+                            <img class="img-fluid" src="../images/camping3.webp" alt="Location Le pal">
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- About End -->
+
+
+<?php include("formulaireContact.php") ?>
+
+
+<?php include("footer.php") ?>
+
+<!-- Google Tag Manager (noscript) -->
+<!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-59DVV3P" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
+<!-- End Google Tag Manager (noscript) -->
+
+ <!-- Google tag (gtag.js) -->
+ <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-M3CJ6F224B"></script> -->
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-M3CJ6F224B');
+    </script>
+    <!-- Google tag (gtag.js) -->
+
+
+
+    <!-- Google Tag Manager -->
+    <!-- <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-59DVV3P');
+    </script> -->
+    <!-- End Google Tag Manager -->
+
+    <!-- Google Analytics -->
+    <script>
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', '32-930029-2', 'auto'); //Id du site par rapport à google analytics, bien mettre à ce format
+        ga('send', 'pageview');
+    </script>
+
+    <script>
+        window.ga = window.ga || function() {
+            (ga.q = ga.q || []).push(arguments)
+        };
+        ga.l = +new Date;
+        ga('create', '32-930029-2', 'auto'); //id du site par rapport à google, toujours le mettre à ce format 
+        ga('send', 'pageview');
+    </script>
+
+    <script async src='https://www.google-analytics.com/analytics.js'></script>
+
+    <!-- End Google Analytics -->
+
+    <!-- Google Tag Manager -->
+    <!-- <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-59DVV3P'); -->
+    </script>
+    <!-- End Google Tag Manager -->
+
+    <!-- Google Analytics -->
+    <script>
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', '32-930029-2', 'auto'); //Id du site par rapport à google analytics, bien mettre à ce format
+        ga('send', 'pageview');
+    </script>
+
+    <script>
+        window.ga = window.ga || function() {
+            (ga.q = ga.q || []).push(arguments)
+        };
+        ga.l = +new Date;
+        ga('create', '32-930029-2', 'auto'); //id du site par rapport à google, toujours le mettre à ce format 
+        ga('send', 'pageview');
+    </script>
+
+    <script async src='https://www.google-analytics.com/analytics.js'></script>
+
+    <!-- End Google Analytics -->
+
+    <!-- Google Tag Manager -->
+    <!-- <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-59DVV3P');
+    </script> -->
+    <!-- End Google Tag Manager -->
+
+    <!-- Google Analytics -->
+    <script>
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', '32-930029-2', 'auto'); //Id du site par rapport à google analytics, bien mettre à ce format
+        ga('send', 'pageview');
+    </script>
+
+    <script>
+        window.ga = window.ga || function() {
+            (ga.q = ga.q || []).push(arguments)
+        };
+        ga.l = +new Date;
+        ga('create', '32-930029-2', 'auto'); //id du site par rapport à google, toujours le mettre à ce format 
+        ga('send', 'pageview');
+    </script>
+
+    <script async src='https://www.google-analytics.com/analytics.js'></script>
+
+    <!-- End Google Analytics -->
+
+    <!-- Balisage JSON-LD généré par l'outil d'aide au balisage de données structurées de Google -->
+    <script type="application/ld+json">
+        {
+            "@context": "http://schema.org",
+            "@type": "LocalBusiness",
+            "name": "Les Caravanes De La Besbre .",
+            "image": "https://lescaravanesdelabesbre.fr/images/banner3.png",
+            "telephone": "06 86 41 31 71"
+        }
+    </script>
+
+   
+    <!-- Google tag (gtag.js) -->
+    <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-69SWVM55LB"></script> -->
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-69SWVM55LB');
+    </script>
+
+</body>
+
+</html>
+
---
+++ .history/public/head_20231221092605.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+<!-- Linking BoxIcon for Icon -->
+<!-- <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'> -->
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" type="image/png" href="../images/logo.png">
+<link href="../css/style.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="icon" type="image/x-icon" href="../images/logo.ico">
