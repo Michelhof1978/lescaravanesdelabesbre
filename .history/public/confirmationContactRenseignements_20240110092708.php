368704e6ece8331e0aea896771b48b99f4063149
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2024-01-10 10:11:06 +0100

    correction erreur vidéo

---
+++ .history/public/confirmationContactRenseignements_20240110092708.php
@@ -0,0 +1,40 @@
+
+<head>
+<!-- Linking BoxIcon for Icon -->
+<!-- <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'> -->
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" type="image/png" href="../images/logo.png">
+<link href="../css/style.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="icon" type="image/x-icon" href="../images/logo.ico">
+<meta name="description" content="Vous souhaitez organiser un séjour au Parc d'Attractions le Pal à plusieurs, possibilité de louer nos 3 caravanes placées côte à côte.">
+<title>Camping Dompierre Sur Besbre Proche Du Pal</title>
+</head>
+
+  <?php include("header.php") ?>
+
+  <section class="confirmation mb-5">
+
+  <h4 class="m-5 text-center border border-3 rounded text-white p-2 display-6 h4Index" id="contact"><strong>MESSAGE ENVOYE</strong></h4>
+  
+  <div class="text-center">
+    <div class="lead text-success display-4 text-center bg-light rounded shadow p-4 pb-5 bg-white rounded ms-1 me-1 mb-5">
+      <p>
+        Nous avons bien reçu votre message et nous vous confirmons que votre demande est en cours de traitement.
+      </p>
+    </div>
+
+  </div>
+
+  </section>
+
+  <?php include("footer.php") ?>
+
+  
+</body>
+
+</html>
