f97ccb0814aadf840af8cb43cfa2240313d1ec29
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2024-01-10 13:55:30 +0100

    amélioration performance

---
+++ .history/public/confirmationContactRenseignements_20240110134208.php
@@ -0,0 +1,28 @@
+<?php include("head.php") ?>
+<head>
+<meta name="description" content="Vous souhaitez organiser un séjour au Parc d'Attractions le Pal à plusieurs, possibilité de louer nos 3 caravanes placées côte à côte.">
+<title>Camping Dompierre Sur Besbre Proche Du Pal</title>
+</head>
+
+
+  <section class="confirmation mb-5">
+
+  <h4 class="m-5 text-center border border-3 rounded text-white p-2 display-6 h4Index" id="contact"><strong>MESSAGE ENVOYE</strong></h4>
+  
+  <div class="text-center">
+    <div class="lead text-success display-4 text-center bg-light rounded shadow p-4 pb-5 bg-white rounded ms-1 me-1 mb-5">
+      <p>
+        Nous avons bien reçu votre message et nous vous confirmons que votre demande est en cours de traitement.
+      </p>
+    </div>
+
+  </div>
+
+  </section>
+
+  <?php include("footer.php") ?>
+
+  
+</body>
+
+</html>
---
+++ .history/public/confirmationContactResa_20240110134249.php
@@ -0,0 +1,34 @@
+<?php include("header.php") ?>
+
+
+<head>
+<?php include("head.php") ?>
+  <meta name="description" content="Vous souhaitez organiser un séjour au Parc d'Attractions le Pal à plusieurs, possibilité de louer nos 3 caravanes placées côte à côte.">
+  <title>Camping Dompierre Sur Besbre Proche Du Pal</title>
+</head>
+
+<?php include("header.php") ?>
+
+
+<section class="confirmation">
+  <h4 class="m-5 text-center border border-3 rounded text-white p-2 display-6 h4Index" id="contact"><strong>MERCI</strong></h4>
+
+  <div class="text-center">
+
+    <img src="./images/logo.png" alt="Le Pal" loading="lazy" class="img-fluid  text-center w-25" />
+
+
+    <div class="lead text-success display-4 text-center bg-light rounded shadow p-4 pb-5 bg-white rounded ms-1 me-1 mb-5">
+      <p>Nous vous remercions sincèrement d'avoir choisi Les Caravanes De La Besbre pour votre prochaine réservation.<br>
+        Nous avons bien reçu votre demande et nous vous confirmons qu'elle est en cours de traitement.
+      </p>
+    </div>
+
+  </div>
+</section>
+
+  <?php include("footer.php") ?>
+
+  </body>
+
+</html>
